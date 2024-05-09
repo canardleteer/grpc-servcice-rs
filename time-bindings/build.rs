c511eea4df15932dfc91aa51951e13ce59d2dc73
canardleteer <eris.has.a.dad+github@gmail.com>
2024-05-09 07:22:24 -0700

    feat: build all protos in the directory

--- time-bindings/build.rs
+++ time-bindings/build.rs
@@ -1,6 +1,9 @@
 use std::{os::unix::fs::PermissionsExt, path::PathBuf};
 
+use walkdir::WalkDir;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_root = "../proto";
     let out_dir = std::env::var("OUT_DIR")?;
     let out_dir_path = std::path::Path::new(&out_dir);
     let metadata = out_dir_path.metadata()?;
@@ -12,6 +15,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         permissions.mode()
     );
 
+    // Grab all the proto files.
+    let mut proto_files = vec![];
+    for entry in WalkDir::new(proto_root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| {
+            e.path()
+                .extension()
+                .map_or(false, |ext| ext.to_str().unwrap() == "proto")
+        })
+    {
+        proto_files.push(entry.path().to_owned());
+    }
+
     // Build a `FileDescriptorSet`
     let descriptor_path = PathBuf::from(out_dir).join("_descriptor.bin");
 
@@ -20,9 +37,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         .generate_default_stubs(true)
         .file_descriptor_set_path(descriptor_path)
-        .compile(
-            &["github/canardleteer/grpc_service_rs/v1alpha1/time.proto"],
-            &["../proto"],
-        )?;
+        .compile(&proto_files, &[proto_root])?;
     Ok(())
 }
